97ec27e6c766e12ce0795d3723db49f50cb5c1fc
“Cassie <cassielee8757@gmail.com>
2022-01-12 16:05:37 +1100

    wed-update

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@
 let gameActive = true;
 let player1 = "O"
 let player2 = "X"
+let playerScore1 = 0;
+let playerScore2 = 0;
+
 // debugger
 //all winning posibilities (first 3 = horizontal , next 3 = vertical , last 2 = diagonal )
 const winningPossibilities = [
@@ -27,56 +30,60 @@ $(function(){
     
     //default player turn 
     let playerTurn = 0;
-
+    
         //game start function 
-        $gameStart = eachBoxes.one('click', function(){
+        const $gameStart = function(){
+            eachBoxes.one('click', function(){
             //call grid id from html using attr
             const gridId = $(this).attr("id");
             //change the string value to integer 
             const gridToNum = parseInt(gridId)
             
             if(playerTurn === 0){
-                //printing visual inputs on the screen 
-                //printing 'O' in the grid 
-                $(this).html(player1);
-                $(this).css('color', '#27005e')
-                
-                //switch player 
+                //printing visual inputs on the grid board 
+                $(this).html(player1); //"O"
+                $(this).css('color', '#27005e') //color input 
+                //switch player1 to player2
                 playerTurn = 1;
-                //player input 1 for switch turns 
+                //fade in and out effect for player1 
                 turn.hide()
                 turn.fadeIn(1000)
+                //player1 input when switch turns 
                 turn.html("Player 1")
-                
-                //push player1 value to the board 
+                //update player1 value('O') to the board 
                 board[gridToNum] = player1;
     
             }else{
-                $(this).html(player2);
-                $(this).css('color', '#7a36a1')
-                //switch player to 0
+                //printing visual inputs on the grid board for player 2
+                $(this).html(player2); // 'X' input 
+                $(this).css('color', '#7a36a1') // color input 
+                //switch player1 to player1 
                 playerTurn = 0;
+                //fade in and out effect for player2
                 turn.hide()
                 turn.fadeIn(1000)
+                //player2 input when switch turns 
                 turn.html("Player 2")
-                
+                //update player2 value('X') to the board 
                 board[gridToNum] = player2;
             }        
             //check winning function runs here 
             checkWinningStatus();
-        });//end of clickevent 
+        });//end of eachBoxes
+        }//end of gameStart 
 
         //winning status function
         const checkWinningStatus = function(){
-            //looping through the winning possibilities array 
+            //loop through the winning possibilities array 
             for (let i = 0; i < winningPossibilities.length; i++){
-                //save the loop into a variable 
+                //save the winning possibilities loop into a variable 
                 const winning = winningPossibilities[i];
-                //let the loop repeats 3 times and save them in a,b,c variables each. 
+                //save winning possibilities loop into 3 different variables = 3 loops 
                 let a = winning[0]
                 let b = winning[1]  
                 let c = winning[2]
-                
+                //place each loop variales with the board array and save them into content a b and
+
                 //call the grid box id from html and loop them
                 let contentA = board[a];
                 let contentB = board[b];
@@ -85,41 +92,46 @@ $(function(){
                 //compare the 3 loop variables to check 
                 if (contentA === contentB && contentB === contentC && contentA !== "") {
                     //if player wins the game turn off click event 
-                    $clickEvent = eachBoxes.off('click');                    
-                    console.log('game over');
-                    turn.html("Game over");
-                    return
+                    $clickEvent = eachBoxes.off('click');  
+
+                    //check who wins the game 
+                    if (playerTurn === 1){
+                        playerScore1++;
+                        $('#score1').html(playerScore1);
+                        turn.html("Player 1 wins");
+
+                    }else{
+                        playerScore2++;                       
+                        $('#score2').html(playerScore2);
+                        turn.html("Player 2 wins");
+
+                    } //end of
 
                 //draw = if the board does not include [''] string then return draw    
                 }else if(!board.includes('')){
-                    console.log('draw');
-                    turn.html("Draw");
-                
-                } //end of if statement 
-                
+                    turn.html("Draw"); // print draw input on the screen     
+                } //end of if statement    
             }// closing for 
         } // function checkwinningstatus
-        
         
         //reset function
         $('.reset-button').on('click', function(){
             //update the board array with empty strings
             board = [ '', '', '', '', '', '', '', '', ''];
             //update them in html 
-            $('.grid-box').html('')
+            $('.grid-box').empty();
             //return to player 1 
             playerTurn = 0;
-            turn.html("CHOOSE THE PLAYER");
+            turn.html("START");//print choose the player on the screen 
+            //return to gameStart
             $gameStart();
+        })//end of resetbutton function
             
-        })
-            
-        $('#playerO').on('click', function(){
-            console.log('clicked')
-        })
+        // $('#playerO').on('click', function(){
+        //     console.log('clicked')
+        // })
     
-            
-        
+    $gameStart(); //starting the function by calling it
 
 })//jquery function
 
